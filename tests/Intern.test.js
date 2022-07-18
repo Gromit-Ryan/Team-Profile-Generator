6bcc69533ad935cdc545276d57326de227b58242
Gromit <eversoryan@gmail.com>
2022-07-18 17:09:15 -0600

    Added test for Intern

---
+++ tests/Intern.test.js
@@ -0,0 +1,45 @@
+const Intern = require('../lib/employee')
+
+const testEmail = 'email@email.com';
+const testID = 03;
+const testName = 'Gromit';
+const testSchool = 'School';
+
+test('Create the intern object', () => {
+    const employee = new Intern(testName, testID, testEmail, testSchool);
+
+    expect(employee.name).toEqual('Gromit');
+    expect(employee.id).toEqual(03);
+    expect(employee.email).toEqual('email@email.com');
+    expect(employee.school).toEqual('School')
+});
+
+test('Retrieve the name', () => {
+    const employee = new Intern(testName, testID, testEmail, testSchool);
+
+    expect(employee.getName()).toEqual(testName);
+});
+
+test('Retrieve the id', () => {
+    const employee = new Intern(testName, testID, testEmail, testSchool);
+
+    expect(employee.getId()).toEqual(testID);
+});
+
+test('Retrieve the email', () => {
+    const employee = new Intern(testName, testID, testEmail, testSchool);
+
+    expect(employee.getEmail()).toEqual(testEmail);
+});
+
+test('Retrieve the github', () => {
+    const employee = new Intern(testName, testID, testEmail, testSchool);
+
+    expect(employee.getschool()).toEqual(testSchool)
+})
+
+test('Retrieve the role', () => {
+    const employee = new Intern(testName, testID, testEmail, testSchool);
+
+    expect(employee.getRole()).toEqual('Intern');
+});
