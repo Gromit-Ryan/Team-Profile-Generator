eb4e76783edc35c412a2d44caf463546d10ace6b
Gromit <eversoryan@gmail.com>
2022-07-18 15:00:32 -0600

    Added function to create engineer card

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -13,3 +13,19 @@ const generateManagerCard = (manager) => {
     </div>
     `
 }
+
+const generateEngineerCard = (engineer) => {
+    return `
+    <div class="card employee-card">
+        <div class="card-header bg-danger text-white">
+            <h3 class="card-title">${engineer.name}</h3>
+            <h4>Manager</h4>
+        </div>
+        <div class="card-body">
+            <p class="id">ID: ${engineer.id}</p>
+            <p class="email">Email <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+            <p class="github"> Github: <a href="https://www.github.com/${engineer.github}" target="_blank">${engineer.github}</a></p>
+        </div>
+    </div>
+    `
+}
